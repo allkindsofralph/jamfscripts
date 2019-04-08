e36403e0ad10ea82776ba306df4be831905aa6bc
Ralph <akoralph@me.com>
2019-04-08 10:09:26 -0700

    Create Examplify-CrashFix.sh

---
+++ Examplify-CrashFix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##################################################################
+# Examplify Crash Fix
+#
+# Fixes issues after a Examplify crash or incorrect shut down
+#
+# Ralph Casafrancisco
+# casafrancisco@chapman.edu
+# 2019-08-April
+
+# Deletes Plist that prevents Desktop items to be seen
+sudo -u $3 rm ~/Library/Preferences/com.apple.finder.plist 
+
+# Deletes Plist that prevents Screen Shots
+sudo -u $3 rm ~/Library/Preferences/com.apple.screencapture.plist
+
+# Tell user that they have 2 minutes before a restart is triggered
+say "You now have 2 minutes to save any documents before a restart is needed"
+sleep 2m
+reboot
