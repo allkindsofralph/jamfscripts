93323c2b377da1e272336bb563ee6ce97535df67
Ralph <akoralph@me.com>
2019-05-10 15:57:47 -0700

    Update askIfDPorGP.sh

--- askIfDPorGP.sh
+++ askIfDPorGP.sh
@@ -19,24 +19,38 @@ accountName="yourAccountName"
 accountPassword="yourAccountPassword"
 jssServerURL="https://my.jss:8443"
 
-# Extension Attribute properties
-eaName="Purchase Type"
-eaName=$(echo "${eaName}" | sed 's/ /%20/g') # Replace spaces with percent encoding
-eaValue="DP"
-
-# Get thew ID of the Extension Attribute specified by name 
-eaID=$( /usr/bin/curl -sku ${accountName}:${accountPassword} -H "Accept: text/xml" ${jssServerURL}/JSSResource/computerextensionattributes/name/${eaName} | xpath '/computer_extension_attribute/id/text()')
-
 # The XML for setting the value of the Extension Attribute
-eaPutXML="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+eaPutXMLDP="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
 <computer>
 	<extension_attributes>
 		<extension_attribute>
-			<id>${eaID}</id>
-			<value>${eaValue}</value>
+			<id>13</id>
+			<value>DP</value>
 		</extension_attribute>
 	</extension_attributes>
 </computer>"
 
-# Set the value of the Extension Attribute
-/usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXML" ${jssServerURL}/JSSResource/computers/udid/${udid}
+eaPutXMLGP="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+<computer>
+	<extension_attributes>
+		<extension_attribute>
+			<id>13</id>
+			<value>GP</value>
+		</extension_attribute>
+	</extension_attributes>
+</computer>"
+
+#Prompt for User Input #### STILL NEEDS EDITING
+while true
+sudo -u $3 do
+purchaseType=$(osascript -e 'tell app "System Events" to display dialog "Is this computer a Departmental/Grant Purchase?" buttons {"DP", "GP", "Neither"} default button 3')
+    if [ $? == "DP" ]     
+    then 
+        /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLDP" ${jssServerURL}/JSSResource/computers/udid/${udid}
+    elif [ $? == "GP" ]
+    then
+        /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLGP" ${jssServerURL}/JSSResource/computers/udid/${udid}
+    else
+        echo "Neither DP or GP"
+    fi
+done
