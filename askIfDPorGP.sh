214a049cd13961b1ec360a227bdf1014e546a2f0
Ralph <akoralph@me.com>
2019-05-11 16:34:40 -0700

    Update askIfDPorGP.sh

--- askIfDPorGP.sh
+++ askIfDPorGP.sh
@@ -40,13 +40,28 @@ eaPutXMLGP="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
 	</extension_attributes>
 </computer>"
 
-#Prompt for User Input #### STILL NEEDS EDITING
+#Prompt for User Input #### STILL NEEDS APPEND DP/GP to Computer Name
 purchaseType=$(osascript -e 'tell app "System Events" to display dialog "Is this computer a Departmental/Grant Purchase?" buttons {"DP", "GP", "Neither"} default button 3')
     if [[ ${purchaseType} == "button returned:DP" ]]; then
+        
+        #API Call to Update Extension Attribute
         /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLDP" ${jssServerURL}/JSSResource/computers/serialnumber/${serial}
+        
+        #Check current computer name if ends with DP
+        checkName=$2
+        
+        #IF STATEMENT HERE
+        
     elif [[ ${purchaseType} == "button returned:GP" ]]; then
+        
+        #API Call to Update Extension Attribute
         /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLGP" ${jssServerURL}/JSSResource/computers/serialnumber/${serial}
+        
+        #Check current computer name if ends with GP
+        checkName=$2
+        
+        #IF STATEMENT HERE
+        
     else
         echo "Neither DP or GP"
-        echo ${purchaseType}
     fi
