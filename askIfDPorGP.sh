98838c73dde8ff0cd71cbfc79b72ce7ca4673795
Ralph <akoralph@me.com>
2019-05-10 17:37:54 -0700

    Update askIfDPorGP.sh

--- askIfDPorGP.sh
+++ askIfDPorGP.sh
@@ -11,13 +11,16 @@
 # 
 # CODE was UTILIZED/CUSTOMIZED from https://configautomation.com/attach-workflow-jamf.html
 
-# UDID of the device is passed to script in environment variable
-udid=$UDID
+# Serial number of the device is passed to script in environment variable
+serial=$(system_profiler SPHardwareDataType | grep 'Serial Number (system)' | awk '{print $NF}')
+
+echo 'hello ${serial}'
+
 
 # Account properties
-accountName="yourAccountName"
-accountPassword="yourAccountPassword"
-jssServerURL="https://my.jss:8443"
+accountName="jssapi"
+accountPassword="RESTEasy!"
+jssServerURL="https://jss.chapman.edu:8443"
 
 # The XML for setting the value of the Extension Attribute
 eaPutXMLDP="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
@@ -41,16 +44,12 @@ eaPutXMLGP="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
 </computer>"
 
 #Prompt for User Input #### STILL NEEDS EDITING
-while true
-sudo -u $3 do
 purchaseType=$(osascript -e 'tell app "System Events" to display dialog "Is this computer a Departmental/Grant Purchase?" buttons {"DP", "GP", "Neither"} default button 3')
-    if [ $? == "DP" ]     
-    then 
-        /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLDP" ${jssServerURL}/JSSResource/computers/udid/${udid}
-    elif [ $? == "GP" ]
-    then
-        /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLGP" ${jssServerURL}/JSSResource/computers/udid/${udid}
+    if [[ ${purchaseType} == "button returned:DP" ]]; then
+        /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLDP" ${jssServerURL}/JSSResource/computers/serialnumber/${serial}
+    elif [[ ${purchaseType} == "button returned:GP" ]]; then
+        /usr/bin/curl -v -sS -k -i -u ${accountName}:${accountPassword} -v -X PUT -H "Content-Type: text/xml" -d "$eaPutXMLGP" ${jssServerURL}/JSSResource/computers/serialnumber/${serial}
     else
         echo "Neither DP or GP"
+        echo ${purchaseType}
     fi
-done
